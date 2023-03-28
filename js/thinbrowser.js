8db17bcc601acd739f313b62624ca844a7c81271
JongHoon Jung <jonghoon93@gmail.com>
2023-03-28 20:57:02 +0900

    add thin browser

---
+++ js/thinbrowser.js
@@ -0,0 +1,14 @@
+document
+  .getElementById("thin_browser_form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // prevent the default form submission behavior
+
+    const winProps =
+      "height=500,width=350,resizable=1,scrollbars=1,status=0,toolbar=1,location=1,menubar=0";
+    const newWin = window.open(
+      document.getElementById("url_for_thin").value,
+      "aWin",
+      winProps
+    );
+    newWin.focus();
+  });
