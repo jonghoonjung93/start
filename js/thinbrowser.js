f3c67332bffca912a4b211bef84439f31f0a10c2
JongHoon Jung <jonghoon93@gmail.com>
2023-04-03 17:35:42 +0900

    thin browser resize

--- js/thinbrowser.js
+++ js/thinbrowser.js
@@ -4,7 +4,7 @@ document
     event.preventDefault(); // prevent the default form submission behavior
 
     const winProps =
-      "height=269,width=330,resizable=1,scrollbars=1,status=0,toolbar=1,location=1,menubar=0";
+      "height=272,width=330,resizable=1,scrollbars=1,status=0,toolbar=1,location=1,menubar=0";
     const newWin = window.open(
       document.getElementById("url_for_thin").value,
       "aWin",
